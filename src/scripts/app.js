61a0a28cc9fc31b78307ccc5121cde9dda3557dd
Martin-GS <contact@martin.gs>
2022-08-09 23:21:33 +0200

    Add Social Network

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,32 +2,35 @@ let data = [
   {
     intro: [
       resume = "Issu d'une école des Beaux-Arts et d'une formation en Développement Web (FullStack JavaScript, avec une spécialisation Front-End), je suis passionné depuis mon plus jeune âge par l'art et les nouvelles technologies ; ce qui m'a amené à me spécialiser dans le développement web et mobile, qui allie technicité, conception et intégration.",
-      portrait = "../images/martings-portrait.png"
+      portrait = "./assets/images/martings-portrait.png"
     ]
   },
   {
     social: [
-      linkedin = [
-        title = "LilnkedIn",
-        url = "https://www.linkedin.com/in/martings",
-        image = "../images/social-linkedin.png"
-      ],
-      twitter = [
-        title = "Twitter",
-        url = "https://twitter.com/__martin_gs__",
-        image = "../images/social-twitter.png"
-      ],
-      instagram = [
-        title = "Instagram",
-        url = "https://www.instagram.com/__martin.gs__",
-        image = "../images/social-instagram.png"
+      text = "#Code #Développement #Web #FrontEnd #BackEnd #FullStack #JavaScript #React #Node #Tech #Travel #Sport",
+      socialnetworks = [
+        {
+          title: "LinkedIn",
+          url: "https://www.linkedin.com/in/martings",
+          image: "./assets/images/social-linkedin.png"
+        },
+        {
+          title: "Twitter",
+          url: "https://twitter.com/__martin_gs__",
+          image: "./assets/images/social-twitter.png"
+        },
+        {
+          title: "Instagram",
+          url: "https://www.instagram.com/__martin.gs__",
+          image: "./assets/images/social-instagram.png"
+        },
       ]
     ]
   },
   {
     repositories: [
       url = "https://github.com/Martin-GS",
-      image = "../images/repositories-github.svg"
+      image = "./assets/images/repositories-github.svg"
     ]
   },
   {
@@ -37,35 +40,35 @@ let data = [
         url_website = "https://martin.gs/",
         url_repository = "https://github.com/Martin-GS/Portfolio",
         text = "Mon portfolio, développeur web spécialisé Front-End",
-        image = "../images/projects-portfolio.svg"
+        image = "./assets/images/projects-portfolio.svg"
       ],
       todojs = [
         title = "ToDo.js",
         url_website = "https://todo.martin.gs",
         url_repository = "https://github.com/Martin-GS/ToDo.js",
         text = "ToDo-list codée en vanilla JavaScript.",
-        image = "../images/projects-todojs.svg"
+        image = "./assets/images/projects-todojs.svg"
       ],
       wishlist = [
         title = "WishList",
         url_website = "https://wishlist.martin.gs",
         url_repository = "https://github.com/Martin-GS/WishList",
         text = "Application web permettant de créer des listes d’articles en important les images depuis les sites marchands.",
-        image = "../images/projects-wishlist.svg"
+        image = "./assets/images/projects-wishlist.svg"
       ],
       inicio = [
         title = "inicio",
         url_website = "https://inicio.martin.gs",
         url_repository = "",
         text = "Page d'accueil personnalisable pour navigateur web.",
-        image = "../images/projects-inicio.svg"
+        image = "./assets/images/projects-inicio.svg"
       ],
       devstack = [
         title = "DevStack",
         url_website = "https://devstack.martin.gs",
         url_repository = "https://github.com/Martin-GS/DevStack",
         text = "Mon carnet de notes de développement web et mobile.",
-        image = "../images/projects-devstack.svg"
+        image = "./assets/images/projects-devstack.svg"
       ]
     ]
   },
@@ -77,27 +80,36 @@ var app = {
    * "structure" sets up the architecture of the page (its skeleton) : header + main + footer 
   */
   structure: function () {
-    app.div = document.createElement('div');
-    app.div.id = 'container';
-    app.header = document.createElement('header');
-    app.header.textContent = "header";
-    app.main = document.createElement('main');
-    app.footer = document.createElement('footer');
-    app.footer.textContent = "footer";
-    document.body.appendChild(app.div);
-    app.div.appendChild(app.header);
-    app.div.appendChild(app.main);
-    app.div.appendChild(app.footer);
+    // container
+    app.containerElement = document.createElement('div');
+    app.containerElement.id = 'container';
+    // header
+    app.headerElement = document.createElement('header');
+    // main
+    app.mainElement = document.createElement('main');
+    // footer
+    app.footerElement = document.createElement('footer');
+    app.footerElement.textContent = "footer";
+    // append-childs
+    document.body.appendChild(app.containerElement);
+    app.containerElement.appendChild(app.headerElement);
+    app.containerElement.appendChild(app.mainElement);
+    app.containerElement.appendChild(app.footerElement);
+  },
+
+  header: function () {
+    // container
+    app.headerElement.textContent = "header";
   },
 
   /**
-   * "structure" sets up the architecture of the page (its skeleton). 
+   * "intro" sets up the résumé and a portrait 
   */
   intro: function () {
-    // section
-    const section = document.createElement('section');
-    section.classList.add('intro');
-    section.id = 'intro';
+    // container
+    const div = document.createElement('div');
+    div.classList.add('intro');
+    div.id = 'intro';
     // résumé
     const resume = document.createElement('p');
     const data_resume = data[0].intro[0];
@@ -105,18 +117,73 @@ var app = {
     // portrait
     const portrait_image = document.createElement('img');
     portrait_image.id = 'portrait';
-    portrait_image.src = './images/martings-portrait.jpg';
+    portrait_image.src = './assets/images/martings-portrait.jpg';
     portrait_image.alt = data[0].intro[1];
+    // append-childs
+    div.appendChild(resume);
+    div.appendChild(portrait_image);
+    app.mainElement.appendChild(div);
+  },
 
-    // appendChild
-    section.appendChild(resume);
-    section.appendChild(portrait_image);
-    app.main.appendChild(section);
+  /**
+   * "social" sets up the social network links
+  */
+  social: function () {
+    // container
+    const div_social = document.createElement('div');
+    div_social.classList.add('social');
+    div_social.id = 'social';
+    div_social.textContent = "Réseaux sociaux";
+    // hashtags
+    const hashtags = document.createElement('p');
+    hashtags.id = 'social-hashtags';
+    hashtags.textContent = data[1].social[0];
+    div_social.appendChild(hashtags);
+    // Image-links
+    const social_links = document.createElement('div');
+    social_links.id = 'social-links';
+    const social_elements = data[1].social[1];
+    social_elements.forEach(item => {
+      // container
+      const div_social_logo = document.createElement('div');
+      div_social_logo.classList = 'social-logo';
+      // link
+      const link_element = document.createElement('a');
+      link_element.href = item.url;
+      // image
+      const image_element = document.createElement('img');
+      image_element.id = `social-logo-${item.title}`;
+      image_element.classList = 'social-logo-image';
+      image_element.src = item.image;
+      image_element.alt = `Lien ${item.title}`;
+      // append-childs
+      link_element.appendChild(image_element);
+      div_social_logo.appendChild(link_element);
+      div_social.appendChild(div_social_logo);
+    });
+    // append-childs
+    app.mainElement.appendChild(div_social);
+  },
+
+  /**
+   *  "projects" sets up the presentation of the projects and their links
+  */
+  projects: function () {
+    // container
+    const div = document.createElement('div');
+    div.classList.add('projects');
+    div.id = 'projects';
+    div.textContent = "Projets";
+    // append-childs
+    app.mainElement.appendChild(div);
   },
 
   init: function () {
     app.structure();
+    app.header();
     app.intro();
+    app.social();
+    app.projects();
   }
 
 };
