86052730264ac8a7c7f41c2aae98b1a91ebcff2a
Martin-GS <contact@martin.gs>
2022-08-12 17:43:41 +0200

    Separation data/code + fix images addresses (manifest.json)

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,81 +1,4 @@
-// ----------  Datas ---------- 
-
-const data = {
-  intro: {
-    name: "Martin GS",
-    position: "Développeur FullStack JavaScript",
-    specialization: "Passionné par le Front-End et spécialisé React.js",
-    location: "Hyères, Côte d'Azur, France",
-    resume: "<p>Issu d'une école des <strong>Beaux-Arts</strong> et d'une formation en développement web et mobile (<strong>FullStack JavaScript</strong>), je suis passionné depuis mon plus jeune âge par l'art et les nouvelles technologies.</p><p>Alliant conception et technicité, j'ai découvert dans le développement <strong>Front-End</strong> une forme d'<i>artisanat</i> où, à partir d'une feuille blanche (ou plutôt un fichier vide), on peut créer une application <i>from scratch</i>.</p><p>La spécialisation Front-End m'a permis de découvrir, en plus de React et son écosystème, des frameworks comme Bootstrap, Bulma et Semantic-UI, le préprocesseur Sass et la syntaxe SCSS, etc.</p><p>En outre, ayant suivi une formation en développement web FullStack, et ayant participé à un projet en tant que Product Owner, j'ai la chance de connaître (et donc de comprendre) le développement <strong>Back-End</strong> (Node, Express, PostgreSQL, SQL, API, bases de données, etc).</p><p>Aujourd'hui je continue à apprendre, chaque jour, et cela me passionne. J'essaie de me tenir informé des nouvelles technologies, mais aussi acquérir de nouvelles compétences.</p>",
-    portrait: "./assets/images/martings-portrait.jpg"
-  },
-  social: {
-    text: "#Code #Développement #Web #FrontEnd #BackEnd #FullStack #JavaScript #React #Node #Tech #Travel #Sport",
-    socialnetworks: [
-      {
-        title: "LinkedIn",
-        url: "https://www.linkedin.com/in/martings",
-        image: "./assets/images/social-linkedin.png",
-        hashtag: "#pro"
-      },
-      {
-        title: "Twitter",
-        url: "https://twitter.com/__martin_gs__",
-        image: "./assets/images/social-twitter.png",
-        hashtag: "#tech"
-      },
-      {
-        title: "Instagram",
-        url: "https://www.instagram.com/__martin.gs__",
-        image: "./assets/images/social-instagram.png",
-        hashtag: "#perso"
-      }
-    ]
-  },
-  repositories: {
-    url: "https://github.com/Martin-GS",
-    image: "./assets/images/repositories-github.svg"
-  },
-  projects: [
-    {
-      title: "Portfolio",
-      url_website: "https://martin.gs/",
-      url_repository: "https://github.com/Martin-GS/Portfolio",
-      text: "Mon portfolio, développeur web spécialisé Front-End",
-      image: "./assets/images/projects-portfolio.svg"
-    },
-    {
-      title: "ToDo.js",
-      url_website: "https://todo.martin.gs",
-      url_repository: "https://github.com/Martin-GS/ToDo.js",
-      text: "ToDo-list codée en vanilla JavaScript.",
-      image: "./assets/images/projects-todojs.svg"
-    },
-    {
-      title: "WishList",
-      url_website: "https://wishlist.martin.gs",
-      url_repository: "https://github.com/Martin-GS/WishList",
-      text: "Application web permettant de créer des listes d’articles en important les images depuis les sites marchands.",
-      image: "./assets/images/projects-wishlist.svg"
-    },
-    {
-      title: "inicio",
-      url_website: "https://inicio.martin.gs",
-      url_repository: "",
-      text: "Page d'accueil personnalisable pour navigateur web.",
-      image: "./assets/images/projects-inicio.svg"
-    },
-    {
-      title: "DevStack",
-      url_website: "https://devstack.martin.gs",
-      url_repository: "https://github.com/Martin-GS/DevStack",
-      text: "Mon carnet de notes de développement web et mobile.",
-      image: "./assets/images/projects-devstack.svg"
-    }
-  ]
-};
-
-// ---------- App ---------- 
+import data from "./data.js";
 
 const app = {
 
@@ -228,16 +151,18 @@ const app = {
   },
 
   /**
-   *  "projects" sets up the presentation of the projects and their links
+   *  "projects" : card-presentation of projects and links
   */
   projects: function () {
     // container
-    const div = document.createElement('div');
-    div.classList.add('projects');
-    div.id = 'projects';
-    div.textContent = "Projets";
-    // append childs
-    app.mainElement.appendChild(div);
+    const projectsDiv = document.createElement('div');
+    projectsDiv.classList.add('projects');
+    projectsDiv.id = 'projects';
+    projectsDiv.textContent = "Projets";
+    app.mainElement.appendChild(projectsDiv);
+    // cards
+    const projectsData = data.projects;
+    console.log(projectsData);
   },
 
   init: function () {
